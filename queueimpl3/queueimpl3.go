5389201a4a5975d0309b1ddfba37623d6f123e16
Christian R. Petrin <38139176+christianrpetrin@users.noreply.github.com>
2018-09-17 18:03:08 -0700

    Update queueimpl3.go

--- queueimpl3/queueimpl3.go
+++ queueimpl3/queueimpl3.go
@@ -83,6 +83,7 @@ func (q *QueueImpl2) Len() int { return q.len }
 
 // Front returns the first element of list l or nil if the list is empty.
 // If the queue is empty, nil is returned.
+// The complexity is O(1).
 func (q *QueueImpl2) Front() interface{} {
 	if q.len == 0 {
 		return nil
@@ -92,6 +93,7 @@ func (q *QueueImpl2) Front() interface{} {
 }
 
 // Push adds a value to the queue.
+// The complexity is O(1).
 func (q *QueueImpl2) Push(v interface{}) error {
 	if v == nil {
 		return errors.New("Cannot add nil value")
@@ -112,6 +114,7 @@ func (q *QueueImpl2) Push(v interface{}) error {
 
 // Pop retrieves and removes the next element from the queue.
 // If the queue is empty, nil is returned.
+// The complexity is O(1).
 func (q *QueueImpl2) Pop() interface{} {
 	if q.len == 0 {
 		return nil
